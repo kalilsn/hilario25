3505765b1b9f95c78a66a7347e82848d4adf2bde
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-22 06:21:54 +0000

    Deploy hilariofor25.com to github.com/kalilsn/hilario25.git:gh-pages

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,10 @@
 $(function(){
   var $form = $('#volunteer-form'),
-      $submit = $form.find('.submit'),
       $error = $form.parent().find('.error'),
       url = 'https://script.google.com/macros/s/AKfycbyfz1cHGt6XQTiIZurutn_Fe3EiOQ1ljHU77tH_x7QQxzGHmBI/exec'
     ;
 
-  $submit.click(function(e) {
+  $form.submit(function(e) {
     var data = {};
     $form.serializeArray().forEach(function(field){
       data[field.name] = field.value;
